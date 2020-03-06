14d016c645f7edc3c5b0db1679afc356297b0d00
Silvia <silviazambrana3@gmail.com>
2020-03-06 19:30:28 +0100

    Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public List<GameObject> floor;
+    public List<GameObject> wall;
+    public List<GameObject> outWall;
+    public List<GameObject> food;
+    public List<GameObject> soda;
+    public GameObject exit;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
